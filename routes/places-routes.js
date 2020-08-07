1c9bcab2f51d5c55a17e3af854c22c40516dc3df
Jorge <jorgediazok@gmail.com>
2020-08-07 13:29:40 -0300

    added express validator

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 const placesControllers = require('../controllers/places-controller');
 
 const router = express.Router();
@@ -7,7 +8,15 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post(
+  '/',
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address').not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
 
 router.patch('/:pid', placesControllers.updatePlace);
 
