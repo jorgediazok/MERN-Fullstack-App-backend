8f45f2784950b9f5eeb6cf3251baf1a28270bc7a
Jorge <jorgediazok@gmail.com>
2020-09-16 16:16:37 -0300

    build added

---
+++ client/build/precache-manifest.59e343e3402cc7426e6d2a781c8ada71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b7f674018c2531482404f3443c0927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7557cf45a5b6305b4731",
+    "url": "/static/css/main.4927ad2c.chunk.css"
+  },
+  {
+    "revision": "584affc003841a3c4d71",
+    "url": "/static/js/2.865a6bec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.865a6bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7557cf45a5b6305b4731",
+    "url": "/static/js/main.8967550f.chunk.js"
+  },
+  {
+    "revision": "5c77e8c54e374416cf77",
+    "url": "/static/js/runtime-main.a14197bb.js"
+  }
+]);
