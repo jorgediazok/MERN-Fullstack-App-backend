3e32234827d524a02a39b9c0985e9c25c278662e
Jorge <jorgediazok@gmail.com>
2020-09-16 16:12:07 -0300

    client added

---
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+import Logo from '../UIElements/location2.png';
+import './MainNavigation.css';
+
+//This is the main navigation
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <div className="navbar-container">
+          <h1 className="main-navigation__title">
+            <Link to="/">I Am Here</Link>
+          </h1>
+          <img src={Logo} alt="logo" className="main-navigation__logo" />
+        </div>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
+export default MainNavigation;
