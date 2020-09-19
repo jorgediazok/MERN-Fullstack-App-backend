1e97350d12541c118f45585cdaf758737ce6447f
Jorge <jorgediazok@gmail.com>
2020-09-19 17:04:23 -0300

    keepon

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -12,7 +12,7 @@ const Users = () => {
     const fetchUsers = async () => {
       try {
         const responseData = await sendRequest(
-          process.env.REACT_APP_BACKEND_URL + '/users'
+          process.env.REACT_APP_BACKEND_URL + '/users/list'
         );
         setLoadedUsers(responseData.users);
       } catch (err) {
